99eec7a354cd83fcd27b26168800c3277a590d38
Nycto <James@RoundEights.com>
2015-02-03 08:24:34 -0800

    Build definition

---
+++ Gruntfile.js
@@ -0,0 +1,75 @@
+/* global module: false */
+
+module.exports = function(grunt) {
+    "use strict";
+
+    // Project configuration.
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+
+        jshint: {
+            files: ['Gruntfile.js', 'src/**/*.js'],
+            options: {
+                bitwise: true,
+                camelcase: true,
+                curly: true,
+                eqeqeq: true,
+                forin: true,
+                immed: true,
+                indent: 4,
+                latedef: true,
+                newcap: true,
+                noarg: true,
+                nonew: true,
+                noempty: true,
+                undef: true,
+                unused: true,
+                strict: true,
+                trailing: true,
+                maxlen: 80,
+                browser: true
+            }
+        },
+
+        uglify: {
+            build: {
+                src: 'src/runway.js',
+                dest: 'build/runway-<%= pkg.version %>.min.js'
+            }
+        },
+
+        simplemocha: {
+            options: {
+                globals: ['should'],
+                timeout: 3000,
+                ignoreLeaks: false,
+                grep: '*-test',
+                ui: 'bdd',
+                reporter: 'tap'
+            },
+
+            all: { src: ['test/**/*.js'] }
+        },
+
+        mochaTest: {
+            test: {
+                src: ['test/**/*.js']
+            }
+        },
+
+        watch: {
+            files: ['<%= jshint.files %>', '<%= mochaTest.test.src %>'],
+            tasks: ['jshint', 'mochaTest']
+        }
+    });
+
+    // Plugins
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-mocha-test');
+
+    // Default task(s).
+    grunt.registerTask('default', ['jshint', 'mochaTest', 'uglify']);
+};
+
